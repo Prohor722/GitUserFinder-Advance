9c207ba18526bb3aa2ec25a941a452fe7ab41e5a
Prohor722 <75115670+Prohor722@users.noreply.github.com>
2021-06-22 02:06:01 +0600

    Add files via upload

---
+++ src/components/users/Test.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+    constructor(){
+        super()
+        this.state={
+            name:""
+        }
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Test
